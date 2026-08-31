717513d241d066ac50f65f7b68c2b963b635f01d
Hemasree28-g <hemasree2827@gmail.com>
2026-08-31 22:45:40 +0530

    day-5

---
+++ 20while3.py
@@ -0,0 +1,6 @@
+i=2
+j=1
+while j<=10:
+    print(i*j)
+
+    j+=1
---
+++ 20while4.py
@@ -0,0 +1,10 @@
+
+nums = [10, 20, 30, 40, 50, 60]
+
+print(len(nums))
+
+idx = 0
+
+while idx <= len(nums):
+    print(nums[idx])
+    idx += 1
